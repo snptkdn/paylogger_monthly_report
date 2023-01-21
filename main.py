c28bd75a4e5003badabcbc101938ec5aa783d609
snptkdn <400m.wflick@gmail.com>
2023-01-21 19:05:47 +0900

    月末か否かを判定する関数を実装した。

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import requests
 import create_amount_per_category
 import create_amount_per_day
 from dotenv import load_dotenv 
+import datetime
+import calendar
 import os
 
-load_dotenv()
-
-DISCORD_MONTHLY_WEBHOOKS_URL = os.getenv('DISCORD_MONTHLY_WEBHOOKS_URL')
+def is_end_of_month(date: datetime.date) -> bool:
+    end_of_month = calendar.monthrange(date.year, date.month)[1]
+    return end_of_month == date.day
 
 def create_monthly_report():
     create_amount_per_day.create()
@@ -25,3 +27,15 @@ def create_monthly_report():
     files = { 'param_name': ('amount_per_category.jpg', image_caregory, 'image/jpeg') }
     data = {'another_key': 'another_value'}
     r = requests.post(DISCORD_MONTHLY_WEBHOOKS_URL, files=files, data=data)
+
+load_dotenv()
+
+DISCORD_MONTHLY_WEBHOOKS_URL = os.getenv('DISCORD_MONTHLY_WEBHOOKS_URL')
+DISCORD_DAILY_WEBHOOKS_URL = os.getenv('DISCORD_DAILY_WEBHOOKS_URL')
+
+if is_end_of_month(datetime.date.today()):
+    create_monthly_report()
+else:
+    data = {'content': '日次レポートをお届けします。'}
+    r = requests.post(DISCORD_DAILY_WEBHOOKS_URL, json=data)
+    
